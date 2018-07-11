006beb1421ae4ecbaa0edbbb08c3b555b24e6b4c
Andrea Vieira <andreavieira@fb.com>
2018-07-11 01:52:15 -0700

    Added RecyclerView for User fragment & User bio.

--- app/src/main/java/andreavieira/parsetagram/HomeFragment.java
+++ app/src/main/java/andreavieira/parsetagram/HomeFragment.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
+    //TimelineAdapter timelineAdapter;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
---
+++ app/src/main/java/andreavieira/parsetagram/TimelineAdapter.java
@@ -0,0 +1,6 @@
+package andreavieira.parsetagram;
+
+import android.support.v7.widget.RecyclerView;
+
+//public class TimelineAdapter extends RecyclerView {
+//}
